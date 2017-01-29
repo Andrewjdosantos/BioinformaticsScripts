56e3b14a2352f9e99e840107b2bc20b66ac06ffc
Andrewjdosantos <andrew@applisential.co.za>
2017-01-29 21:09:27 +0200

    test

--- StringReconstruction.py
+++ StringReconstruction.py
@@ -4,10 +4,10 @@ import random
 from collections import Counter
 import Overlap
 import EulerianPath
-lines = sys.stdin.read().splitlines() # read in the input from STDIN
+# lines = sys.stdin.read().splitlines() # read in the input from STDIN
 
-k = int(lines[0])
-kmers = lines[1:len(lines)]
+# k = int(lines[0])
+# kmers = lines[1:len(lines)]
 
 def StringReconstruction(k,kmers):
 	adjacency = Overlap.Adjacency(kmers)
@@ -19,4 +19,13 @@ def StringReconstruction(k,kmers):
 		string = string + EulerianPList[i][k-1]
 	return(string)
 
-print(StringReconstruction(k,kmers))
+
+def Binary(k):
+	lst = list(itertools.product([0, 1], repeat=k))
+	binList = []
+	for i in range(0,len(lst)):
+		binList.append("".join(str(x) for x in lst[i]))
+	eul = StringReconstruction(k,binList)
+	return(eul)
+
+print(Binary(4))
